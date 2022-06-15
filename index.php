5210638a78eaf8776fe371e1d482db581655229e
katyakatysha <muhortovaekaterina35@gmail.com>
2022-06-15 21:54:20 +0300

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+//массив 1 способ
+
+$array = array(1, 2, 5, 'jskghjk', false);
+
+//массив 2 способ
+
+$array = array[1, 2, 5, 'jskghjk', false];
+
+
+
---
+++ index2.php
@@ -0,0 +1,195 @@
+<?php
+
+
+$a = 100;
+$b = 10;
+//арифметические операторы
+//echo $a+$b+10+30;
+//echo "<br>";
+//
+//echo $a-$b;
+//echo "<br>";
+//
+//echo $a*$b;
+//echo "<br>";
+//echo var_dump($a/$b);
+//echo "<br>";
+//echo $a%$b; //остаток от деления
+//echo $a**$b;
+
+//оператор ветвления
+// $a=true;
+// if ($a){
+//   echo "it's true";
+// }
+
+ //отрицание в операторе ветвления (!) и знак и (&&)
+// $a=false;
+// $b=true;
+//if ($a && $b){
+//    echo "it's true";
+//}
+
+//$a=false;
+//$b=true;
+//if (!$a && !$b){
+//    echo "it's false";
+//}
+
+//оператор или (||)
+// $a=false;
+// $b=true;
+//if ($a || $b){
+//    echo "it's true";
+//}
+
+//оператор или (xor)
+//$a=false;
+//$b=true;
+//if ($a xor $b){
+//    echo "it's true";
+//}
+
+//$a = true;
+//$b = true;
+//$c = null;
+//$arr = array ();
+//$c111 = -1;
+//$float = '0'; //преобразует в false
+//if ($float){
+//    echo "it's true";
+//    } elseif($a) //лучше писать вместе, но можно и раздельно
+//{
+//    echo "ololo"
+//}
+//elseif($b){
+//    "kjdsjf"
+//}else($c){
+//    echo "slgjl"
+//}
+
+//строки
+//$a = 'dsjkg';
+//$string = "dsklfj $a"; //интерполяция (вставляется значение) (чтобы не сливалось, можно {$a})
+//$string2 = 'dskjf $a'; //просто текст
+//
+//echo $string;
+//echo "<br>";
+//echo $string2;
+//
+////сложение строк
+//
+//$a = 'VAR';
+//$string = "string1";
+//$string2 = 'string2';
+//
+//echo $string."+".$string2;
+
+//операторы сравнения
+
+$a=10;
+$b = 10.0;
+//if ($a == $b){
+//    echo "ok";
+//}else{
+//    echo "!ok";
+//}
+
+//if ($a === $b)//тождественное равенство
+//    {
+//    echo "ok";
+//}else{
+//    echo "!ok";
+//}
+
+//if ($a !== $b){
+//    echo "ok";
+//}else{
+//    echo "!ok";
+//}
+
+//операторы присваивания (инкрементного)
+//$a = 1;
+//$a += 4; //$a +=4;
+//echo $a;
+
+//$a = 0;
+//
+//if ($a++){
+//    echo "ok";
+//}else{
+//    echo "!ok";
+//}
+
+//декрементного (++a)
+
+//1
+//$rec = rand (0, 100);
+//echo $rec;
+//echo "<br>";
+// if ($rec %2 == 0){
+//    echo "chet";
+//}else{
+//   echo "nechet";
+//}
+////2
+//echo "<br>";
+//if (0<= $rec && $rec <=10){
+//    echo "Диапазон 1";
+//}elseif(10< $rec && $rec <=40){
+//    echo "Диапазон 2";
+//}elseif(40< $rec && $rec <=80){
+//    echo "Диапазон 3";
+//}elseif(80< $rec && $rec <=100){
+//    echo "Диапазон 4";
+//}else{
+//    echo "Не входит в диапазон";
+//}
+
+//вложенные условия
+//$rand = 48;
+//$day = 5;
+//if ($rand >= 0 && $rand<=50){
+//    if ($day >=1 &&$rand<=5){
+//        $rand *=1.15;
+//    }else{
+//        rand *=1.30
+//    }
+//}else{
+//
+//}
+
+//Сравнение строк
+//$string1 = 'ololo';
+//$string2 = 'ololo';
+//var_dump($string1 ==$string2);
+
+//$str1 = '1';
+//$str2 = '3';
+//
+//$str1 = intval ($str1);
+//var_dump($str1);
+//
+//$str2 = floatval($str2);
+//var_dump ($str2);
+//var_dump(boolval('121'));
+//
+//var_dump(intval());
+//var_dump((int)$str+(int)$str2);
+
+//..константа (1 вариант)
+//define ('MY CONST', 111111);
+//echo MY_CONST;
+//
+////константа (2 вариант)
+//const MY_CONST2 = 22222;
+//echo "<br>";
+//echo MY_CONST2;
+
+
+
+
+
+
+
+
